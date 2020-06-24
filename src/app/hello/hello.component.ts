bf88852675c4d14f0bdaeecb67884eeb8f50b6dc
AngelAbraham <angel-07061@hotmail.com>
2020-06-24 18:40:20 -0500

    Commit Input con eventos

--- src/app/hello/hello.component.ts
+++ src/app/hello/hello.component.ts
@@ -10,11 +10,24 @@ export class HelloComponent{
     @Input() name:string;
     @Output() sayHello:EventEmitter<string> = new EventEmitter<string>();
 
+    public nameList: Array<string>=[];
+    public inputName:string='Jhon';
     constructor(){
     }
 
     onNameClick(){
         this.sayHello.emit('Hello!');
+    }
+
+    addName(){
+        this.nameList.push(this.inputName);
+        this.inputName='';
+        console.log(this.nameList);
+    }
+
+    updateInputName(event:any){
+        this.inputName = event.target.value;
+        console.log(this.inputName);
     }
 
 }
