d7f1d3143964b4ec8c655e1930c29d732e23ac56
AngelAbraham <angel-07061@hotmail.com>
2020-06-24 17:43:14 -0500

    Commit segundo componente

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,10 +4,12 @@ import { NgModule } from '@angular/core';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { MiComponente} from './components/mi-componente/mi-componente.component';
+import { HelloComponent } from './hello/hello.component';
 @NgModule({
   declarations: [
     AppComponent,
-    MiComponente
+    MiComponente,
+    HelloComponent
   ],
   imports: [
     BrowserModule,
